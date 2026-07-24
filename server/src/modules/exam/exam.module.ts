9674f699a12411748fd72bbe33f786e9f6f0d32c
shilun <xushilun_w@outlook.com>
2026-07-24 17:53:35 +0800

    refactor(stats, exam, question): 导出并引入统计模块
    将StatsModule添加导出，让考试和题目模块可以导入使用该模块

--- server/src/modules/exam/exam.module.ts
+++ server/src/modules/exam/exam.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExamController } from './exam.controller';
 import { ExamService } from './exam.service';
+import { StatsModule } from '../stats/stats.module';
 
 @Module({
+  imports: [StatsModule],
   controllers: [ExamController],
   providers: [ExamService],
 })
--- server/src/modules/question/question.module.ts
+++ server/src/modules/question/question.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { QuestionController } from './question.controller';
 import { QuestionService } from './question.service';
 import { DbModule } from '@/db/db.module';
+import { StatsModule } from '../stats/stats.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, StatsModule],
   controllers: [QuestionController],
   providers: [QuestionService],
   exports: [QuestionService],
